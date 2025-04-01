dedf628be098bf106c9f2b2e293abfa20e8cbd9a
João José Bentivi <jjbentivi@hotmail.com>
2025-03-31 22:56:18 -0300

    Update visualizardados.py

--- visualizardados.py
+++ visualizardados.py
@@ -81,11 +81,16 @@ else:
             st.dataframe(contagem_acoes)
 
             # Gráfico de barras para a frequência das ações
-            plt.style.use('dark_background')
-            fig1, ax1 = plt.subplots(figsize=(10, 6))
-            sns.barplot(x='Contagem', y='Acao', data=contagem_acoes, ax=ax1)
-            ax1.set_title("Frequência de Ações")
-            st.pyplot(fig1)
+            #estilos = [ 'dark_background', 'fast', 'fivethirtyeight','Solarize_Light2', '_classic_test_patch', '_mpl-gallery', '_mpl-gallery-nogrid',]
+            estilos = [ 'bmh', 'classic', 'ggplot', 'grayscale', 'seaborn-v0_8', 'seaborn-v0_8-bright', 'seaborn-v0_8-colorblind', 'seaborn-v0_8-dark', 'seaborn-v0_8-dark-palette', 'seaborn-v0_8-darkgrid', 'seaborn-v0_8-deep', 'seaborn-v0_8-muted', 'seaborn-v0_8-notebook', 'seaborn-v0_8-paper', 'seaborn-v0_8-pastel', 'seaborn-v0_8-poster', 'seaborn-v0_8-talk', 'seaborn-v0_8-ticks', 'seaborn-v0_8-white', 'seaborn-v0_8-whitegrid', 'tableau-colorblind10']
+            for estilo in estilos:
+                plt.style.use(estilo)
+                st.text(f"Estilo utilizado: {estilo}")
+                fig1, ax1 = plt.subplots(figsize=(10, 6))
+                sns.barplot(x='Contagem', y='Acao', data=contagem_acoes, ax=ax1)
+                ax1.set_title("Frequência de Ações")
+                st.pyplot(fig1)
+                plt.close(fig1)  # Fecha a figura para liberar memória
 
             # 2. Evolução Temporal das Ações (Diária)
             st.subheader("Evolução Temporal das Ações")
