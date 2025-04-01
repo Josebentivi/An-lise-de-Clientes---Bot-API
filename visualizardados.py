e72ff3d1a379bba2b89273411e42981598ab0928
João José Bentivi <jjbentivi@hotmail.com>
2025-04-01 14:41:38 -0300

    Update visualizardados.py

--- visualizardados.py
+++ visualizardados.py
@@ -185,7 +185,9 @@ else:
             contagem_acoes = df['Acao_Agrupada'].value_counts().reset_index()
             contagem_acoes.columns = ['Acao', 'Contagem']
             st.dataframe(contagem_acoes)
-            contagem_acoes = contagem_acoes[['Consulra PDF chat utilizado', 'Consulta Vade utilizada', 'Consulta Servidor utilizada', 'Consulta Doutrina utilizada', 'Consulta JurisBrasil utilizada', 'Consulta Leis utilizada', 'Criar documentação utilizada', 'Audio utilizado',  'Voz utilizada', 'Imagem carregada',  'PDF enviada',  'PDF enviado', 'Papo utilizado', 'Consulta removida', 'Consulta adicionada',  'Bateria removida', 'Bateria adicionada', 'Documentação utilizada',  'Vade utilizado', 'CGU utilizada',  'Doutrina utilizada', 'Leis utilizada',  'Consulta PDF utilizada', 'Bibliografia utilizada']]
+
+            ordered_actions = ['Consulra PDF chat utilizado', 'Consulta Vade utilizada', 'Consulta Servidor utilizada', 'Consulta Doutrina utilizada', 'Consulta JurisBrasil utilizada', 'Consulta Leis utilizada', 'Criar documentação utilizada', 'Audio utilizado', 'Voz utilizada', 'Imagem carregada', 'PDF enviada', 'PDF enviado', 'Papo utilizado', 'Consulta removida', 'Consulta adicionada', 'Bateria removida', 'Bateria adicionada', 'Documentação utilizada', 'Vade utilizado', 'CGU utilizada', 'Doutrina utilizada', 'Leis utilizada', 'Consulta PDF utilizada', 'Bibliografia utilizada']
+            contagem_acoes = contagem_acoes.set_index("Acao").reindex(ordered_actions, fill_value=0).reset_index()
             
 
             # Gráfico de barras para a frequência das ações
