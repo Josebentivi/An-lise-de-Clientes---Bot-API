0255fa2c56871da93d57f63297caa847a130161c
João José Bentivi <jjbentivi@hotmail.com>
2025-04-01 13:16:37 -0300

    Update visualizardados.py

--- visualizardados.py
+++ visualizardados.py
@@ -43,7 +43,7 @@ else:
     # Leitura dos dados
     #df = pd.read_csv(uploaded_file)
     df = pd.DataFrame(saida,columns=["Usuario","Data","Fonte","Obs","Créditos Função 1","Créditos Função 2"])
-    df = df["Usuario","Data","Créditos Função 1","Créditos Função 2"]
+    df = df[["Usuario","Data","Créditos Função 1","Créditos Função 2"]]
     
     st.subheader("Visão Geral dos Dados")
     st.dataframe(df.head())
